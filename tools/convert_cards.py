602342e25edb5b42dc51316ffc463c0d8c4b491d
webninjasi <webninjasi.0@gmail.com>
2022-07-19 07:54:54 +0300

    Add card/cell data and converter tool

---
+++ tools/convert_cards.py
@@ -0,0 +1,40 @@
+#!/usr/bin/py -3
+
+import csv
+
+head = []
+cards = []
+lines = []
+
+with open('cards.csv', newline='') as csvfile:
+  reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+  cards = []
+  
+  for row in reader:
+    cards += [row]
+  
+  head = cards[0]
+  cards = cards[1:]
+  
+  lines += [ 'monopoly.config.board.cells = {' ]
+  
+  for card in cards:
+    lines += [ '  {' ]
+    
+    for idx, value in enumerate(card):
+      if idx < 2:
+        lines += [ f'    {head[idx]} = "{value}",' ]
+      else:
+        try:
+          int(value)
+          lines += [ f'    {head[idx]} = {value},' ]
+        except:
+          pass
+    
+    lines += [ '  },' ]
+  
+  
+  lines += [ '}' ]
+
+with open('cards.lua', 'w') as luafile:
+  luafile.write('\n'.join(lines))
